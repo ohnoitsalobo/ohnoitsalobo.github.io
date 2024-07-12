9cee5e0516f92840ece488edd2d249e4d761320a
ohnoitsalobo <alobo001@odu.edu>
2024-07-12 08:25:21 +0530

    update

--- noteCircle/new/js/script.js
+++ noteCircle/new/js/script.js
@@ -344,7 +344,7 @@ const imageOrder = [ 0, 1, 3, 4, 6, 7, 8, 10, 11, 13, 14, 16, 2, 5, 9, 12, 15];
 
 function loadScales(){
     // let begin = "<object width='50%' type='image/svg+xml' data='modes/svg/";
-    let mid = "<div id='img'><div class='imgText'><div style='margin-top: 5px;'>Treble (G clef)</div><div style='margin-top: 55px;'>Alto (C clef)</div><div style='margin-top: 57px;'>Bass (F clef)</div><div style='margin-top: 58px;'>Mandolin tablature</div><div style='margin-top: 66px;'>Guitar tablature</div></div>";
+    let mid = "<div id='img'><div class='imgText'><div style='margin-top: 7px;'>Treble (G clef)</div><div style='margin-top: 59px;'>Alto (C clef)</div><div style='margin-top: 59px;'>Bass (F clef)</div><div style='margin-top: 61px;'>Mandolin tablature</div><div style='margin-top: 69px;'>Guitar tablature</div></div>";
     let begin = mid + "<object class='img' type='image/svg+xml' data='../modes/svg/";
     let end = ".svg' style='filter: invert(1);'>Please use a web browser with SVG image support to see the image.</object>";
     for(let j = 0; j < 17; j++){
